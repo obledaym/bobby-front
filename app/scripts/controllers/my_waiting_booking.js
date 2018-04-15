1213758e545392581a7e5f81a9589dea3fb714ab
obledaym <aymeric.obled@weezevent.com>
2018-04-15 05:10:30 +0200

    add my booking views

---
+++ app/scripts/controllers/my_waiting_booking.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bobbyApp.controller:MainCtrl
+ * @description
+ * # ItemAddCtrl
+ * Controller of the bobbyApp
+ */
+angular.module('bobbyApp')
+  .controller('MyWaitingBookingCtrl', function ($scope, serviceAjax, $routeParams) {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    var id = $routeParams.asso_id;
+     console.log(id);
+    var loadAssociations = function(){
+
+            $scope.loading = true;
+          serviceAjax.path("associations").then(function(data){
+                console.log("Poulet3",data.data);
+
+
+            });
+        };
+
+
+        loadAssociations();
+    /*var loadBookings = function(){
+            $scope.loading = true;
+          serviceAjax.path("bookings").then(function(data){
+                console.log("Poulet 2",data.data.data);
+
+
+            });
+        };
+
+
+        loadBookings();*/
+  });
