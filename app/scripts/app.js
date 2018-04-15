d1e47d8ef6433ac24e31c8ae565267aeb90d73e4
obledaym <aymeric.obled@weezevent.com>
2018-04-15 04:30:20 +0200

    add booking views

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -24,11 +24,29 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
-      })
-        .when('/items/:id', {
+      }).when('/items/:id', {
         templateUrl: 'views/items.html',
         controller: 'ItemsCtrl',
         controllerAs: 'items'
+      }).when('/add_item/:asso_id', {
+        templateUrl: 'views/add_item.html',
+        controller: 'AddItemCtrl',
+        controllerAs: 'addItem'
+      })
+        .when('/validated_booking/:asso_id', {
+        templateUrl: 'views/validated_booking.html',
+        controller: 'ValidatedBookingCtrl',
+        controllerAs: 'validatedBooking'
+      })
+        .when('/waiting_booking/:asso_id', {
+        templateUrl: 'views/waiting_booking.html',
+        controller: 'WaitingBookingCtrl',
+        controllerAs: 'waitingBooking'
+      })
+        .when('/refused_booking/:asso_id', {
+        templateUrl: 'views/refused_booking.html',
+        controller: 'RefusedBookingCtrl',
+        controllerAs: 'refusedBooking'
       })
       .otherwise({
         redirectTo: '/'
